fc35511568b19e92dbb164ae85b804a0c2205f37
tim kaspar <tim2kaspar@gmail.com>
2021-09-29 11:37:52 +0200

    http request works in main

--- lib/main.dart
+++ lib/main.dart
@@ -33,56 +33,54 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: ElevatedButton(
-                      onPressed: () {
-                        Future entries = getAllEntries();
-                        print(entries);
-                      }, child: const Text('Get all'))),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'get');
-                    },
-                    child: const Text('Get')),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'delete');
-                    },
-                    child: const Text('Delete')),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'create');
-                    },
-                    child: const Text('Create')),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'put');
-                    },
-                    child: const Text('Put')),
-              ),
-            ],
+      child: Column(
+        children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                  onPressed: () {
+                    Future entries = getAllEntries();
+                    print(entries);
+                  },
+                  child: const Text('Get all'))),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, 'get');
+                },
+                child: const Text('Get')),
           ),
-        ));
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, 'delete');
+                },
+                child: const Text('Delete')),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, 'create');
+                },
+                child: const Text('Create')),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, 'put');
+                },
+                child: const Text('Put')),
+          ),
+        ],
+      ),
+    ));
   }
 }
 
 Future<http.Response> getAllEntries() async {
-  //TODO most probably CORS errord
-  return await http.get(Uri.parse(
-      'http://localhost:8080/entries'), headers:{"Access-Control-Allow-Origin": "*"}
-  );
+  return await http.get(Uri.parse("http://localhost:8080/entries"));
 }
