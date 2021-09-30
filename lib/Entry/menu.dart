9f4b15e277df97dadc14ec935583b88a2d8c7cb9
Tim <33898974+TimKaspar@users.noreply.github.com>
2021-09-30 14:24:59 +0200

    moved entry menu from main.dart => menu.dart

---
+++ lib/Entry/menu.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MyHomePage extends StatelessWidget {
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: AlignmentDirectional.topCenter,
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.only(top: 50),
+            child: Text(
+              'Entry',
+              style: TextStyle(fontSize: 40),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.all(10),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          Future entries = getAllEntries();
+                          print(entries);
+                        },
+                        child: Text('Get all'))),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'get');
+                      },
+                      child: Text('Get')),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'delete');
+                      },
+                      child: Text('Delete')),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'create');
+                      },
+                      child: Text('Create')),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, 'put');
+                      },
+                      child: Text('Put')),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: textEditingController,
+                    maxLines: null,
+                    minLines: 6,
+                    keyboardType: TextInputType.multiline,
+                    enabled: false,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Future<http.Response> getAllEntries() async {
+  return await http.get(Uri.parse("http://localhost:8080/entries"));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'Entry/create.dart';
 import 'Entry/delete.dart';
 import 'Entry/get.dart';
+import 'Entry/menu.dart';
 import 'Entry/put.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -23,89 +23,4 @@ class MyApp extends StatelessWidget {
       },
     );
   }
-}
-
-class MyHomePage extends StatelessWidget {
-
-  final TextEditingController textEditingController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        alignment: AlignmentDirectional.topCenter,
-        children: <Widget>[
-          const Padding(
-            padding: EdgeInsets.only(top: 50),
-            child: Text(
-              'Entry',
-              style: TextStyle(fontSize: 40),
-            ),
-          ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                      padding: EdgeInsets.all(10),
-                      child: ElevatedButton(
-                          onPressed: () {
-                            Future entries = getAllEntries();
-                            print(entries);
-                          },
-                          child: Text('Get all'))),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, 'get');
-                        },
-                        child: Text('Get')),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, 'delete');
-                        },
-                        child: Text('Delete')),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, 'create');
-                        },
-                        child: Text('Create')),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, 'put');
-                        },
-                        child: Text('Put')),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: TextField(
-                      controller: textEditingController,
-                      maxLines: null,
-                      minLines: 6,
-                      keyboardType: TextInputType.multiline,
-                      enabled: false,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-        ],
-      ),
-    );
-  }
-}
-
-Future<http.Response> getAllEntries() async {
-  return await http.get(Uri.parse("http://localhost:8080/entries"));
 }
