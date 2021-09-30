3a538bc0c78018798d834c9a475920f4966e686f
Tim <33898974+TimKaspar@users.noreply.github.com>
2021-09-30 14:30:33 +0200

    create login_menu.dart

---
+++ lib/login/login_menu.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoginMenu extends StatelessWidget {
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+                padding: EdgeInsets.all(10),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '');
+                    },
+                    child: Text('Login'))),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '');
+                  },
+                  child: Text('Register')),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
