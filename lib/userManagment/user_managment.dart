e413517de27892ef57e28fbb08a7fc713674c0ed
Tim <33898974+TimKaspar@users.noreply.github.com>
2021-09-30 15:45:17 +0200

    fixed IncorrectParent error

--- lib/userManagment/user_managment.dart
+++ lib/userManagment/user_managment.dart
@@ -4,42 +4,40 @@ import 'package:flutter/material.dart';
 class UserManagment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        width: 700,
-        child: ListView.builder(
-          scrollDirection: Axis.vertical,
-          shrinkWrap: true,
-          itemBuilder: (context, index) {
-            return Card(
-              elevation: 8.0,
-              child: ListTile(
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                title: Row(
-                  children: <Widget>[
-                    Text('Username'),
-                    Text(' '),
-                    Text('Password'),
-                  ],
-                ),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.edit),
-                      onPressed: () {},
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
+    return SizedBox(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: 20,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 8.0,
+            child: ListTile(
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              title: Row(
+                children: <Widget>[
+                  Text('Username'),
+                  Text(' '),
+                  Text('Password'),
+                ],
               ),
-            );
-          },
-        ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
